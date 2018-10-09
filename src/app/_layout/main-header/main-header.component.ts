b29066dec50f39800e1ae60bcd5c6c75b510b6bb
vovarybak <vova_rybak@ukr.net>
2018-10-09 22:03:52 +0300

    changes

--- src/app/_layout/main-header/main-header.component.ts
+++ src/app/_layout/main-header/main-header.component.ts
@@ -2,6 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import {Languages} from '../../mocks/languages';
 import {animate, keyframes, query, stagger, style, transition, trigger} from '@angular/animations';
 import {TranslateService} from '@ngx-translate/core';
+import {AuthService} from '../../services/auth.service';
+import {of} from 'rxjs/internal/observable/of';
+import {skipWhile} from 'rxjs/operators';
+import {BehaviorSubject} from 'rxjs/BehaviorSubject';
 
 @Component({
   selector: 'carryFest-main-header',
@@ -11,9 +15,13 @@ import {TranslateService} from '@ngx-translate/core';
 export class MainHeaderComponent implements OnInit {
   public languagesList;
   public chosenLanguage;
-  constructor(private translate: TranslateService) {
+  public userData;
+  public _userData: BehaviorSubject<any>;
+  constructor(private translate: TranslateService, public authService: AuthService) {
     this.languagesList = Languages.LanguagesList;
     this.chosenLanguage = this.translate.currentLang;
+    this._userData = this.authService._authData;
+    this._userData.subscribe((userInfo) => this.userData = userInfo);
     this.translate.onLangChange.subscribe((lang) => {
       this.chosenLanguage = lang.lang;
       localStorage.setItem('language', lang.lang);
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -2,15 +2,19 @@ import { Injectable } from '@angular/core';
 import { AngularFireAuth } from 'angularfire2/auth';
 import {BehaviorSubject} from 'rxjs/BehaviorSubject';
 import {Router} from '@angular/router';
+import {FirebaseService} from './firebase.service';
+import {of} from 'rxjs/internal/observable/of';
 // import * as firebase from 'firebase/app';
 
 @Injectable()
 export class AuthService {
-  public _authData: any;
-  constructor(private afAuth: AngularFireAuth, private router: Router) { }
+  public _authData: BehaviorSubject<any> = new BehaviorSubject<any>({});
+  constructor(private afAuth: AngularFireAuth, private router: Router, private fireBaseService: FirebaseService) {
+    this.initAuthState();
+  }
   initAuthState(): void {
-    const userInfo = localStorage.userInfo;
-    this._authData = userInfo ? JSON.parse(userInfo) : null;
+    const userInfo = JSON.parse(localStorage.userInfo);
+    this._authData.next( userInfo ? userInfo : null);
   }
   private storeAuthData(userInfo: any): void{
     const userData = Object.assign(JSON.parse(localStorage.getItem('userInfo')), userInfo);
@@ -18,8 +22,20 @@ export class AuthService {
   }
   setAuthData(user: any): void {
     this.storeAuthData(user);
-    this.initAuthState();
+    this.getUserAllInfo(user.userID);
     this.router.navigate(['me/profile']);
+  }
+  private getUserAllInfo(ID: string) {
+    this.fireBaseService.getUserByID(ID).on('value', (value) => {
+      const queryResult = value.val();
+      const key = Object.keys(queryResult);
+      this.storeAuthData(queryResult[key[0]]);
+      this.initAuthState();
+    });
+  }
+  public logOut() {
+    localStorage.removeItem('userInfo');
+    this._authData.next( null);
   }
   get isLoggedIn(): boolean {
     return !!this._authData;
